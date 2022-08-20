c325efb3399cc0cbfdeceaddfa110b9166c1fc2e
numtel <ben@latenightsketches.com>
2022-08-19 20:09:25 -0700

    Medians recorded in epochs

--- contracts/HumanToken.sol
+++ contracts/HumanToken.sol
@@ -7,12 +7,15 @@ import "./IWorldID.sol";
 import "./ByteHasher.sol";
 
 contract HumanToken is ERC20 {
+  string public name = "Human Token";
+  string public symbol = "HUM";
+  uint8 public decimals = 6;
+
   using ByteHasher for bytes;
 
   mapping(address => uint8) public accountBallots;
   uint64[32] public ballotBuckets;
   uint public ballotCount;
-  uint8 public ballotMedian;
 
   IWorldID public worldId;
   uint public groupId;
@@ -20,13 +23,19 @@ contract HumanToken is ERC20 {
   uint public epochDuration;
   uint public initTime;
 
+  struct EpochMedian {
+    uint128 epochNumber;
+    uint8 median;
+  }
+  EpochMedian[] public epochs;
+
   mapping(uint256 => bool) internal nullifierHashes;
 
   mapping(address => uint) public lastCollected;
 
   constructor(
     IWorldID _worldId,
-    uint256 _groupId,
+    uint256 _groupId, // always 1
     string memory _actionId,
     uint _epochDuration
   ) {
@@ -50,6 +59,8 @@ contract HumanToken is ERC20 {
 
     require(initBallot > 0 && initBallot < 33);
     accountBallots[msg.sender] = initBallot;
+    ballotBuckets[initBallot]++;
+    ballotCount++;
     updateMedian();
 
     worldId.verifyProof(
@@ -63,6 +74,36 @@ contract HumanToken is ERC20 {
   }
 
   function updateMedian() internal {
+    uint128 curEpoch = uint128((block.timestamp - initTime) / epochDuration);
+
+
+    // Calculate the median from all the buckets
+    uint8 curBucket;
+    uint medianPos = ballotCount / 2;
+    bool countIsEven = ballotCount % 2 == 0;
+    uint soFar;
+    uint8 otherBucket;
+    while(soFar <= medianPos) {
+      soFar += ballotBuckets[curBucket];
+      curBucket++;
+      if(countIsEven && soFar == medianPos && otherBucket == 0) {
+        // Median is between 2 buckets
+        otherBucket = curBucket;
+      }
+    }
+    if(otherBucket > 0) {
+      curBucket = (otherBucket + curBucket) / 2;
+    }
+
+
+    if(epochs.length > 0 && epochs[epochs.length - 1].epochNumber == curEpoch) {
+      // This epoch already has an entry
+      epochs[epochs.length - 1].median = curBucket - 1;
+    } else {
+      // The median has not changed in this epoch yet
+      epochs.push(EpochMedian(curEpoch, curBucket - 1));
+    }
+
   }
 
   function submitBallot(uint8 value) external {
--- test/HumanToken.test.js
+++ test/HumanToken.test.js
@@ -1,18 +1,21 @@
 const assert = require('assert');
 
 exports.canJoin = async function({
-  web3, accounts, deployContract, loadContract, throws, BURN_ACCOUNT,
+  web3, accounts, deployContract, loadContract, throws, BURN_ACCOUNT, increaseTime,
 }) {
+  const EPOCH_DURATION = 30;
+
   const mockWorldId = await deployContract(accounts[0], 'MockWorldID');
   const token = await deployContract(accounts[0], 'HumanToken',
     mockWorldId.options.address,
     1, // groupId
     "test", // actionId
-    30 // epochDuration
+    EPOCH_DURATION // epochDuration
   );
 
   await mockWorldId.sendFrom(accounts[0]).setValid(1);
 
+  // First user joins
   await token.sendFrom(accounts[0]).join(
     1, // root
     2, // nullifierHash
@@ -22,5 +25,57 @@ exports.canJoin = async function({
 
   const ballotValue = await token.methods.accountBallots(accounts[0]).call();
   assert.strictEqual(Number(ballotValue), 3);
+
+  let curEpoch = await token.methods.epochs(0).call();
+  assert.strictEqual(Number(curEpoch.median), 3);
+
+  // Cannot reuse nullifierHash
+  assert.strictEqual(await throws(() =>
+    token.sendFrom(accounts[1]).join(
+      1, // root
+      2, // nullifierHash
+      [0,0,0,0,0,0,0,0], // proof
+      5 // initBallot
+    )), true);
+
+  // Second user joins
+  await token.sendFrom(accounts[1]).join(
+    1, // root
+    3, // nullifierHash
+    [0,0,0,0,0,0,0,0], // proof
+    5 // initBallot
+  );
+
+  // Median is updated
+  curEpoch = await token.methods.epochs(0).call();
+  assert.strictEqual(Number(curEpoch.median), 4);
+
+  // Third user joins
+  await token.sendFrom(accounts[1]).join(
+    1, // root
+    4, // nullifierHash
+    [0,0,0,0,0,0,0,0], // proof
+    9 // initBallot
+  );
+
+  curEpoch = await token.methods.epochs(0).call();
+  assert.strictEqual(Number(curEpoch.median), 5);
+
+  await increaseTime(EPOCH_DURATION + 1);
+
+  // Fourth user joins
+  await token.sendFrom(accounts[1]).join(
+    1, // root
+    5, // nullifierHash
+    [0,0,0,0,0,0,0,0], // proof
+    9 // initBallot
+  );
+
+  // Previous epoch stays the same
+  const prevEpoch = await token.methods.epochs(0).call();
+  assert.strictEqual(Number(prevEpoch.median), 5);
+  // New epoch is recorded
+  curEpoch = await token.methods.epochs(1).call();
+  assert.strictEqual(Number(curEpoch.median), 7);
 }
 
